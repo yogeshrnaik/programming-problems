93064032fd3e39b4d393156432af190dc982f055
Yogesh Naik <yogeshrnaik@gmail.com>
2018-05-23 10:13:59 +0530

    alternate way of implementing SMS count - Code not fully ready

--- src/com/programming/splitsms/Solution.java
+++ src/com/programming/splitsms/Solution.java
@@ -2,11 +2,83 @@ package com.programming.splitsms;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
 
+    private static class SmsResult {
+
+        private int smsCount;
+        private final int limit;
+        private String smsSoFar;
+        private boolean smsTxtBiggerThanLimit;
+
+        public SmsResult(int limit) {
+            this.limit = limit;
+        }
+
+        public SmsResult append(String txt) {
+            if (smsTxtBiggerThanLimit || txt.length() > limit) {
+                smsCount = -1;
+                smsTxtBiggerThanLimit = true;
+                return this;
+            }
+
+            String sms = smsSoFar + " " + txt;
+            if (sms.length() > limit) {
+                smsCount++;
+                smsSoFar = txt;
+            } else {
+                smsSoFar = smsSoFar + " " + txt;
+            }
+            return this;
+        }
+
+        public SmsResult merge(SmsResult sms) {
+            return this;
+        }
+
+    }
+
+    private static int getSmsCount(String S, int limit) {
+        List<String> words = Arrays.asList(S.split(" "));
+
+        SmsResult smsResult = words.stream().reduce(new SmsResult(limit),
+            (sms, txt) -> sms.append(txt),
+            (sms1, sms2) -> sms1.merge(sms2));
+
+        AtomicBoolean cannotBeDone = new AtomicBoolean(false);
+        final AtomicInteger smsCount = new AtomicInteger(1);
+
+        words.stream().reduce((s1, s2) -> {
+            String sms = s1 + " " + s2;
+            if (sms.length() >= limit) {
+                smsCount.incrementAndGet();
+
+                if (sms.length() == limit) {
+                    return "";
+                } else {
+                    if (s2.length() > limit) {
+                        cannotBeDone.set(true);
+                    }
+                    return s2;
+                }
+
+            }
+            return sms;
+        });
+
+        if (words.size() == 1 && S.length() > limit) {
+            cannotBeDone.set(true);
+        }
+
+        return cannotBeDone.get() ? -1 : smsCount.get();
+    }
+
     public int solution(String S, int K) {
-        return new SmsMessageSplitter(S, K).getSmsCount();
+        return getSmsCount(S, K);
+        // return new SmsMessageSplitter(S, K).getSmsCount();
     }
 
     private static final class SmsMessageSplitter {
