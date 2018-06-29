7737dda243be7cdf666ed70bbcb7f35d8beeaef3
Yogesh Naik <yogeshrnaik@gmail.com>
2018-06-29 12:18:05 +0530

    refactoring

--- src/com/programming/altassian/Base7Conversion.java
+++ src/com/programming/altassian/Base7Conversion.java
@@ -20,10 +20,15 @@ public class Base7Conversion {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            long number = -1;
-            while ((number = scanner.nextLong()) != -1) {
-                System.out.println("Number: " + number + " to base7 = " + convertToBase7(number));
-            }
+            convertFromDecimalToBase7(scanner);
+            convertFromBase7ToDecimal(scanner);
+        }
+    }
+
+    private static void convertFromDecimalToBase7(Scanner scanner) {
+        long number = -1;
+        while ((number = scanner.nextLong()) != -1) {
+            System.out.println("Number: " + number + " to base7 = " + convertToBase7(number));
         }
     }
 
@@ -35,5 +40,21 @@ public class Base7Conversion {
             result = base7.get(remainder) + result;
         }
         return result;
+    }
+
+    public static void convertFromBase7ToDecimal(Scanner scanner) {
+        System.out.println("Enter number in base7 to convert it to Decimal. Enter -1 to exit.");
+        String number = "";
+        while (!(number = scanner.next()).equals("-1")) {
+            System.out.println("Number from base7: " + number + " to decimal = " + convertFromBase7ToDecimal(number));
+        }
+    }
+
+    private static long convertFromBase7ToDecimal(String number) {
+        long sum = 0;
+        for (int i = 0; i < number.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(number.charAt(i))) * Math.pow(7, number.length() - i - 1);
+        }
+        return sum;
     }
 }
