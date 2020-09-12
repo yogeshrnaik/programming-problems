231d08f4256682353ae89834fbfb0e9239520049
Yogesh Naik <yogeshnaik@Yogeshs-MacBook-Pro.local>
2020-09-12 14:01:15 +0530

    Booking Solution 3

--- src/com/programming/booking/problem3/Solution.java
+++ src/com/programming/booking/problem3/Solution.java
@@ -8,6 +8,38 @@ public class Solution {
     static void find_all_possible_teams() {
         try (Scanner s = new Scanner(System.in)) {
             String team = s.nextLine();
+            print_all_teams(team);
+        }
+    }
+
+
+    static void print_all_teams(String team) {
+        for (int i = 0; i < team.length(); i++) {
+            System.out.println(team.charAt(i));
+        }
+
+        for (int i = 0; i < team.length() - 1; i++) {
+            for (int j = i + 1; j < team.length(); j++) {
+                System.out.println(team.charAt(i) + "" + team.charAt(j));
+            }
+        }
+
+        for (int i = 0; i < team.length() - 2; i++) {
+            for (int j = i + 1; j < team.length() - 1; j++) {
+                for (int k = j + 1; k < team.length(); k++) {
+                    System.out.println(team.charAt(i) + "" + team.charAt(j) + "" + team.charAt(k));
+                }
+            }
+        }
+
+        for (int i = 0; i < team.length() - 3; i++) {
+            for (int j = i + 1; j < team.length() - 2; j++) {
+                for (int k = j + 1; k < team.length() - 1; k++) {
+                    for (int l = k + 1; l < team.length(); l++) {
+                        System.out.println(team.charAt(i) + "" + team.charAt(j) + "" + team.charAt(k) + "" + team.charAt(l));
+                    }
+                }
+            }
         }
     }
 
