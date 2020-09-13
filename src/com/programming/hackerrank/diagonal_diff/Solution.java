8573bf899773c890cb568edd13b1a4bc62139b81
Yogesh Naik <yogeshnaik@Yogeshs-MacBook-Pro.local>
2020-09-13 12:47:56 +0530

    Added FizzBuzz sample HackerRank solution

--- src/com/programming/hackerrank/diagonal_diff/Solution.java
+++ src/com/programming/hackerrank/diagonal_diff/Solution.java
@@ -1,25 +1,36 @@
 package com.programming.hackerrank.diagonal_diff;
 
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * https://www.hackerrank.com/challenges/diagonal-difference/problem
  */
-public class Solution {
+class Result {
 
-    // Complete the diagonalDifference function below.
-    static int diagonalDifference(int[][] arr) {
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
         int primary = 0;
         int secondary = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.get(i).size(); j++) {
                 if (i == j) {
-                    primary += arr[i][j];
+                    primary += arr.get(i).get(j);
                 }
-                if (i + j == arr.length - 1) {
-                    secondary += arr[i][j];
+                if (i + j == arr.size() - 1) {
+                    secondary += arr.get(i).get(j);
                 }
             }
         }
@@ -27,34 +38,35 @@ public class Solution {
         return Math.abs(primary - secondary);
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
+}
 
+public class Solution {
     public static void main(String[] args) throws IOException {
-        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        int[][] arr = new int[n][n];
+        List<List<Integer>> arr = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < n; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
-        }
+        });
 
-        int result = diagonalDifference(arr);
-        System.out.println(result);
+        int result = Result.diagonalDifference(arr);
 
-        // bufferedWriter.write(String.valueOf(result));
-        // bufferedWriter.newLine();
-        //
-        // bufferedWriter.close();
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
 
-        scanner.close();
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
