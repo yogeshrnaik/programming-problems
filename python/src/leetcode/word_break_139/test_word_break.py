76326493aff09e3bbff03488469a3d8432961314
Yogesh Naik <yogeshrnaik@gmail.com>
2022-08-15 20:10:52 +0530

    Update test_word_break.py

--- python/src/leetcode/word_break_139/test_word_break.py
+++ python/src/leetcode/word_break_139/test_word_break.py
@@ -6,7 +6,7 @@ from leetcode.word_break_139.word_break import Solution
 class TestWordBreak(unittest.TestCase):
 
     def test_word_break(self):
-        # self.assertEqual(True, Solution().wordBreak("leetcode", ["leet", "code"]))
-        # self.assertEqual(True, Solution().wordBreak("applepenapple", ["apple", "pen"]))
-        # self.assertEqual(False, Solution().wordBreak("catsandog", ["cats","dog","sand","and","cat"]))
+        self.assertTrue(Solution().wordBreak("leetcode", ["leet", "code"]))
+        self.assertTrue(Solution().wordBreak("applepenapple", ["apple", "pen"]))
+        self.assertFalse(Solution().wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]))
         self.assertFalse(Solution().wordBreak("a", ["b"]))
