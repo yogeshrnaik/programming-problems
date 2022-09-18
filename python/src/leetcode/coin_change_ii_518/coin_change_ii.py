07cde659d32f42c43e76cfb7d3b7eacd455f9bcd
Yogesh Naik <yogeshrnaik@gmail.com>
2022-09-18 22:51:36 +0530

    coin-change-ii with 1-d array

--- python/src/leetcode/coin_change_ii_518/coin_change_ii.py
+++ python/src/leetcode/coin_change_ii_518/coin_change_ii.py
@@ -15,4 +15,35 @@ class Solution:
             memo[(index, curr_amount)] = dfs(index, curr_amount + coins[index]) + dfs(index + 1, curr_amount)
             return memo[(index, curr_amount)]
 
-        return dfs(0, 0)
+        result = dfs(0, 0)
+
+        self.dynamic_programming(amount, coins)
+
+        return self.change2(amount, coins)
+
+    def dynamic_programming(self, amount: int, coins: List[int]):
+        dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
+        dp[0] = [1] * (len(coins) + 1)
+
+        for a in range(1, amount + 1):
+            for i in range(len(coins) - 1, -1, -1):
+                dp[a][i] = dp[a][i + 1]
+                if a - coins[i] >= 0:
+                    dp[a][i] += dp[a - coins[i]][i]
+
+        self.print(dp)
+
+    def print(self, dp):
+        for i in range(len(dp)):
+            for j in range(len(dp[i])):
+                print(dp[i][j], "\t", end="")
+            print("\n")
+
+    def change2(self, amount: int, coins: List[int]) -> int:
+        cache = [0] * (amount + 1)
+        cache[0] = 1
+        for coin in coins:
+            for index in range(1, amount + 1):
+                if index - coin >= 0:
+                    cache[index] += cache[index - coin]
+        return cache[amount]
--- python/src/leetcode/coin_change_ii_518/test_coin_change_ii.py
+++ python/src/leetcode/coin_change_ii_518/test_coin_change_ii.py
@@ -6,7 +6,7 @@ from leetcode.coin_change_ii_518.coin_change_ii import Solution
 class TestCoinChange(unittest.TestCase):
 
     def test_coin_change(self):
-        # self.assertEqual(1, Solution().change(10, [10]))
-        # self.assertEqual(0, Solution().change(3, [2]))
+        self.assertEqual(1, Solution().change(10, [10]))
+        self.assertEqual(0, Solution().change(3, [2]))
         self.assertEqual(4, Solution().change(5, [1, 2, 5]))
-        # self.assertEqual(3, Solution().change(3, [1, 2]))
+        self.assertEqual(2, Solution().change(3, [1, 2]))
