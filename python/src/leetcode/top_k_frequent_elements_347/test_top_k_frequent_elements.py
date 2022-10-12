d2d920b16909b18c8001e0d4fd35a1e0b1848354
Yogesh Naik <yogeshrnaik@gmail.com>
2022-10-12 12:52:38 +0530

    Update test_top_k_frequent_elements.py

--- python/src/leetcode/top_k_frequent_elements_347/test_top_k_frequent_elements.py
+++ python/src/leetcode/top_k_frequent_elements_347/test_top_k_frequent_elements.py
@@ -3,8 +3,8 @@ import unittest
 from leetcode.top_k_frequent_elements_347.top_k_frequent_elements import Solution
 
 
-class TestUniquePathsWithObstacles(unittest.TestCase):
+class TestTopKFrequent(unittest.TestCase):
 
-    def test_union_paths(self):
+    def test_top_k_frequents(self):
         self.assertListEqual([1, 2], Solution().topKFrequent(nums=[1, 1, 1, 2, 2, 3], k=2))
         self.assertListEqual([1], Solution().topKFrequent(nums=[1], k=1))
