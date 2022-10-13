36a5481533994bf0faa404eac4674347f189fdff
Yogesh Naik <yogeshrnaik@gmail.com>
2022-10-13 11:42:32 +0530

    Update Solution.java

--- java/src/com/programming/leetcode/lowest_common_ancestor_236/Solution.java
+++ java/src/com/programming/leetcode/lowest_common_ancestor_236/Solution.java
@@ -16,17 +16,22 @@ class Solution {
     TreeNode result = null;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//        findLCA(root, p, q);
-//        return result;
         if (root == null) return null;
         if (root.val == p.val || root.val == q.val) return root;
 
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if (left != null && right != null) return root; // split happens at this node
+        // if both left and right are not null,
+        // this means p and q lies in left and right part of the tree from current root node
+        if (left != null && right != null) return root;
 
+        // left not null means, both p and q are part of left sub-tree
         return (left != null) ? left : right;
+
+        /* Alternate approach */
+        // findLCA(root, p, q);
+        // return result;
 
     }
 
