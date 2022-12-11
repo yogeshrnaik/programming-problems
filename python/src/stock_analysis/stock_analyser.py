0ddbbd2723ecab3d8ed7677e599b5c07e054f13d
Yogesh Naik <yogeshrnaik@gmail.com>
2022-12-11 16:05:41 +0530

    write_analysed_stock_holdings

--- python/src/stock_analysis/stock_analyser.py
+++ python/src/stock_analysis/stock_analyser.py
@@ -2,9 +2,9 @@ import csv
 import json
 
 STOCK_CATEGORY = {
-    "CORE": ["HINDUNILVR", "ITC", "RELIANCE", "TCS", "SBIN"],
+    "CORE": ["HDFCBANK", "HINDUNILVR", "ITC", "RELIANCE", "TCS", "SBIN"],
     "STRONG-NON-CORE": ["EUREKAFORBE", "IRFC", "MAXHEALTH", "MAXVIL", "POONAWALLA"],
-    "OTHER-NON-CORE": ["IDEA", "ISMTLTD", "MADHAVBAUG-SM", "MAFANG", "RENUKA", "SHREERAMA", "TTML"]
+    "OTHER-NON-CORE": ["HCC", "HEMIPROP", "IDEA", "ISMTLTD", "MADHAVBAUG-SM", "MAFANG", "RENUKA", "SHREERAMA", "TTML"]
 }
 
 
@@ -39,11 +39,41 @@ def group_by_category(holdings):
     return holdings_by_category
 
 
+def stats_by_category(holdings_by_category):
+    category_stats = {}
+    for category, category_holdings in holdings_by_category.items():
+        stat = category_stats.get(category, {})
+        stat[f"Cur. val"] = category_stats.get(f"{category}-Cur. val", 0) \
+                            + sum(float(h["Cur. val"]) for h in category_holdings)
+        stat[f"INVESTED"] = category_stats.get(f"{category}-INVESTED", 0) \
+                            + sum(float(h["Qty."]) * float(h["Avg. cost"]) for h in category_holdings)
+        category_stats[category] = stat
+
+    for category, stat in category_stats.items():
+        stat["% of 50L"] = 100 * stat.get("INVESTED") / 5000000
+    return category_stats
+
+
+def write_analysed_stock_holdings(holdings_by_category, category_stats):
+    for category, holdings in holdings_by_category.items():
+        for h in holdings:
+            invested = float(h['Qty.']) * float(h['Avg. cost'])
+            percentage_of_50lacs = 100 * invested / 5000000
+            print(
+                f"{category},{h['Instrument']},{h['Qty.']},{h['Avg. cost']},{invested},{h['Cur. val']},{h['P&L']},{percentage_of_50lacs}")
+
+        # print sub total of category
+        print(f"{category},,,,{category_stats[category]['INVESTED']},,,{category_stats[category]['% of 50L']}")
+
+
 def analyse_stock_holdings(holdings):
     categorise_holdings(holdings)
     holdings.sort(key=lambda h: h["category"])
     holdings_by_category = group_by_category(holdings)
     print(json.dumps(holdings_by_category, sort_keys=True, indent=2))
+    category_stats = stats_by_category(holdings_by_category)
+    print(json.dumps(category_stats, sort_keys=True, indent=2))
+    write_analysed_stock_holdings(holdings_by_category, category_stats)
 
 
 def print_holdings(holdings):
