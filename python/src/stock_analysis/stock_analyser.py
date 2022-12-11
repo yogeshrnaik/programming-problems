56eee18c6dbbfc65cfc292a17b5f1c56948ace48
Yogesh Naik <yogeshrnaik@gmail.com>
2022-12-11 16:28:24 +0530

    print sub-total and global-total

--- python/src/stock_analysis/stock_analyser.py
+++ python/src/stock_analysis/stock_analyser.py
@@ -43,10 +43,12 @@ def stats_by_category(holdings_by_category):
     category_stats = {}
     for category, category_holdings in holdings_by_category.items():
         stat = category_stats.get(category, {})
-        stat[f"Cur. val"] = category_stats.get(f"{category}-Cur. val", 0) \
-                            + sum(float(h["Cur. val"]) for h in category_holdings)
-        stat[f"INVESTED"] = category_stats.get(f"{category}-INVESTED", 0) \
-                            + sum(float(h["Qty."]) * float(h["Avg. cost"]) for h in category_holdings)
+        stat["Cur. val"] = category_stats.get(f"{category}-Cur. val", 0) \
+                           + sum(float(h["Cur. val"]) for h in category_holdings)
+        stat["INVESTED"] = category_stats.get(f"{category}-INVESTED", 0) \
+                           + sum(float(h["Qty."]) * float(h["Avg. cost"]) for h in category_holdings)
+        stat["P&L"] = category_stats.get(f"{category}-P&L", 0) \
+                      + sum(float(h["P&L"]) for h in category_holdings)
         category_stats[category] = stat
 
     for category, stat in category_stats.items():
@@ -55,15 +57,26 @@ def stats_by_category(holdings_by_category):
 
 
 def write_analysed_stock_holdings(holdings_by_category, category_stats):
+    global_total = {}
+
     for category, holdings in holdings_by_category.items():
         for h in holdings:
             invested = float(h['Qty.']) * float(h['Avg. cost'])
             percentage_of_50lacs = 100 * invested / 5000000
             print(
-                f"{category},{h['Instrument']},{h['Qty.']},{h['Avg. cost']},{invested},{h['Cur. val']},{h['P&L']},{percentage_of_50lacs}")
+                f"{category},{h['Instrument']},{h['Qty.']},{h['Avg. cost']},{invested},{h['LTP']},{h['Cur. val']},{h['P&L']},{percentage_of_50lacs}%")
 
         # print sub total of category
-        print(f"{category},,,,{category_stats[category]['INVESTED']},,,{category_stats[category]['% of 50L']}")
+        category_stat = category_stats[category]
+        print(f"Sub-Total,,,,{category_stat['INVESTED']},,{category_stat['Cur. val']},{category_stat['P&L']},{category_stat['% of 50L']}%")
+        print(f",,,,,,,")
+
+        # calculate global total
+        global_total["INVESTED"] = global_total.get("INVESTED", 0) + category_stat['INVESTED']
+        global_total["Cur. val"] = global_total.get("Cur. val", 0) + category_stat['Cur. val']
+        global_total["P&L"] = global_total.get("P&L", 0) + category_stat['P&L']
+
+    print(f"Grand-Total,,,,{global_total['INVESTED']},,{global_total['Cur. val']},{global_total['P&L']},")
 
 
 def analyse_stock_holdings(holdings):
